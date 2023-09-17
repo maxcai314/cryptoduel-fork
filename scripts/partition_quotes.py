8e3945b8a96a88e808db643ae5b24813086c1a4a
Max Cai <maxcai.paloalto@gmail.com>
2023-09-17 00:05:32 -0700

    generated new quotes. public/quotes/0.json contains the funny quotes

--- scripts/partition_quotes.py
+++ scripts/partition_quotes.py
@@ -8,19 +8,27 @@ from pathlib import Path
 root = Path(__file__).parent / "../public/quotes"
 src = Path(__file__).parent / "../src"
 
-with open(root / "all.json", "r") as fin:
-    quotes = json.load(fin)
+with open(root / "all.json", "r") as fin_real:
+    realQuotes = json.load(fin_real)
+
+with open(root / "funny.json", "r") as fin_funny:
+    funnyQuotes = json.load(fin_funny)
 
 chunk_size = 500
 amount_chunks = 0
 
-for i in range(len(quotes) // chunk_size + 1):
-    with open(root / f"{i}.json", "w+") as fout:
-        json.dump(quotes[i * chunk_size:(i + 1) * chunk_size], fout)
+for i in range(len(funnyQuotes) // chunk_size + 1):
+    with open(root / f"{amount_chunks}.json", "w+") as fout:
+        json.dump(funnyQuotes[i * chunk_size:(i + 1) * chunk_size], fout)
+    amount_chunks += 1
+
+for i in range(len(realQuotes) // chunk_size + 1):
+    with open(root / f"{amount_chunks}.json", "w+") as fout:
+        json.dump(realQuotes[i * chunk_size:(i + 1) * chunk_size], fout)
     amount_chunks += 1
 
 with open(src / "meta.js", "w+") as fout:
     print(f"""
-export const amountQuotes = {len(quotes)};
+export const amountQuotes = {len(realQuotes) + len(funnyQuotes)};
 export const amountChunks = {amount_chunks};
 """.lstrip(), file=fout)
---
+++ src/meta.js
@@ -0,0 +1,3 @@
+export const amountQuotes = 5452;
+export const amountChunks = 12;
+
